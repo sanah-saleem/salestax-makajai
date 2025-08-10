556ee8f430a2c460539ddd568c8e020bd9a59aab
sanah saleem <sanahsaleem2000@gmail.com>
2025-08-10 17:28:11 +0530

    first commit

---
+++ src/main/java/com/assignment/SalesTax/SalesTaxApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.SalesTax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesTaxApplication.class, args);
+	}
+
+}
